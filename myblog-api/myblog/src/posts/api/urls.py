fda3bf3969b08687621abf93a7217392b8dc4aae
shubhanshu-gupta <shubhanshu2007@gmail.com>
2016-10-05 10:03:07 +0530

    Retrieve API view

--- myblog-api/myblog/src/posts/api/urls.py
+++ myblog-api/myblog/src/posts/api/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 
 from .views import (
 	PostListAPIView,
+	PostDetailAPIView,
 	)
 
 urlpatterns = [
 	url(r'^$', PostListAPIView.as_view(), name='list'),
     # url(r'^create/$', post_create),
-    # url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    url(r'^(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='detail'),
     # url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
     # url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
--- myblog-api/myblog/src/posts/api/views.py
+++ myblog-api/myblog/src/posts/api/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from posts.models import *
 from .serializers import *
 
 class PostListAPIView(ListAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
+
+class PostDetailAPIView(RetrieveAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
