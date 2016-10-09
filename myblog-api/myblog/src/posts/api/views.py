2e7fbc08d45c05abd9f0a17c75cbaca19e4f5d92
shubhanshu-gupta <shubhanshu2007@gmail.com>
2016-10-09 22:18:19 +0530

    defined permissions along with a custom permission

--- myblog-api/myblog/src/posts/api/views.py
+++ myblog-api/myblog/src/posts/api/views.py
@@ -7,12 +7,21 @@ from rest_framework.generics import (
 	RetrieveUpdateAPIView
 	)
 
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
+	)
+
 from posts.models import *
 from .serializers import *
+from .permissions import *
 
 class PostCreateAPIView(CreateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
+	permission_classes = [IsAuthenticated]
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user)
@@ -32,6 +41,9 @@ class PostDeleteAPIView(DestroyAPIView):
 class PostUpdateAPIView(RetrieveUpdateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
+	#IsOwnerOrReadOnly ensures that the users who is being updated is the owner
+	#That is the user who owns the object
+	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 	def perform_update(self, serializer):
 		serializer.save(user=self.request.user)
